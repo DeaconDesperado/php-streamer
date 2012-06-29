9146a17a97bb550d21f8ca53d3f0df9effb9dbd0
Mark Grey <mark@mrak.(none)>
2012-06-29 11:17:39 -0400

    added example

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php
+//CHANGE THESE VALUES FOR YOUR OWN EXAMPLE
+define('REMOTE_HOST','streamer.loc');
+define('REMOTE_PORT','8081')
+
 echo $_SERVER['REQUEST_METHOD'];
 if($_SERVER['REQUEST_METHOD'] =='GET'){
 ?>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script type="text/javascript" src="/xhr.js"></script>
+   <h1>Socket forwarder example</h1>
 
-    <form method="post" action="<?=$_SERVER['PHP_SELF'];?>" enctype="multipart/form-data"/>
-        <input type="file" name="uploaded_file"/>
-        <button type="submit">Send</button>
-    </form>
-    
+   <p>Uses a combination of HTTP put and the <code>php://input</code> wrapper to stream a file to a remote host over a socket</p>
+   
+   <p>Please note you will need to set up hosts to run this example</p>
+
+   <ol>
+        <li>Make sure python and the python requests module are installed: <code>sudo pip install requests</code></li>
+        <li>Set the hosts up, most likely at your localhost.  There are constants in the php file.  You will need to setup hosts in your hosts
+                file as well.</li>
+        <li>Run remote_server.py.  This is just an example service that will listen indefinitely on a socket on the machine it is run on.  It represents the remote
+        service you'll be forwarding to.</li>
+        <li>Run put.py.  This will fire an http request sending the included image file to whatever host you setup for the php.  The php
+        will then open a socket to remote_server and stream it there... you should see the bytes of the image in your terminal for remote_server.py!</li>
+   <ol>
 <?
 }else{
 
-    $host = 'streamer.loc';
-    $port = '8081';
+    $host = REMOTE_HOST;
+    $port = REMOTE_PORT;
 
     $socket = socket_create(AF_INET,SOCK_STREAM,0) or die('Could not get socket');
     $bind = socket_connect($socket,$host,$port) or die('Could not bind socket');
-
-    
 
     $fh = fopen('php://input','r');
     while(!feof($fh)){
